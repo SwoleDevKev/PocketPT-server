2c20b960d9d8fba58e0146a280e21acef01ffed7
Kevin Aristhomene <swoledevkev@gmail.com>
2024-01-10 18:38:36 -0500

    removed unused constraint in exercise and trainer tables

--- migrations/20231124183111_create_trainers_table.js
+++ migrations/20231124183111_create_trainers_table.js
@@ -5,7 +5,6 @@
 exports.up = function(knex) {
     return knex.schema.createTable('trainers', (table) => {
         table.increments('id').primary();
-        table.integer('program_id').unsigned().references('programs.id');
         table.string('trainer_avatar');
         table.string('first_name').notNullable();
         table.string('last_name').notNullable();
