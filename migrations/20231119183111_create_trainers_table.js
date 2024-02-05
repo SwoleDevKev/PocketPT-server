668beaf1d3eb324396360acd4d3a8bbf0ee099f3
Kevin Aristhomene <swoledevkev@gmail.com>
2024-02-04 20:04:49 -0500

    changed migration file order to accomodate foreign key constraint

---
+++ migrations/20231119183111_create_trainers_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('trainers', (table) => {
+        table.increments('id').primary();
+        table.string('trainer_avatar');
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.string('phone').notNullable().unique();
+        table.string('email').notNullable().unique();
+        table.string('password').notNullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('trainers');
+
+};
